73ec1a5da6805d7f0758b96a681e6fa93ac3e664
Ajonye Paul <ajonyechase@gmail.com>
2024-03-31 14:27:43 +0100

    initial commit. project up and running with tailwind CSS installed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className=" bg-pink-400 p-5">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis asperiores molestias esse nisi minima ratione
+			eligendi repudiandae odit dolore quae. Ut quae expedita provident et tempore natus facilis ipsum quis.
+		</div>
+	);
+}
+
+export default App;
